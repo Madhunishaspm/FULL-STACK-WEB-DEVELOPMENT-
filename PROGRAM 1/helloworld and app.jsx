f946c0bc119d9424fe056ea9747fcd0f40707896
Madhunishaspm <madhunishaparameshwaran@gmail.com>
2025-12-11 00:14:25 +0530

    Create helloworld and app.jsx

---
+++ PROGRAM 1/helloworld and app.jsx
@@ -0,0 +1,15 @@
+ 1. App.jsx
+
+import HelloWorld from "./core concepts/helloworld/helloworld"
+
+function App(){
+  return <HelloWorld/>
+}
+export default App
+  
+2. HelloWorld.jsx
+
+ export default function HelloWorld()
+{
+    return <h1>Hello world</h1>;
+}
